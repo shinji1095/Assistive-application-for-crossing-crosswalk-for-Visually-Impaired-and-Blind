2a0fc0587f03a8c8529a5cef56988a7785f232ea
shinji1095 <shinji1095nameko@gmail.com>
2023-07-30 22:30:30 +0900

    add: camera real-time preview

--- lib/parts/dobjImageWidget.dart
+++ lib/parts/dobjImageWidget.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:app_a/main.dart' as main;
 
-class DObjImageWidget extends StatelessWidget {
-  const DObjImageWidget({super.key});
+final _cameras = main.cameras;
+
+class DObjImageWidget extends StatefulWidget{
+  const DObjImageWidget({Key? key}) : super(key: key);
+
+  @override
+  State<DObjImageWidget> createState() => _DObjImageWidget();
+}
+
+class _DObjImageWidget extends State<DObjImageWidget>{
+  late CameraController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_){
+      if (!mounted){
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e){
+      if (e is CameraException){
+        switch (e.code){
+          case 'CameraAccessDenied':
+            print('User denied camera access');
+            break;
+          default:
+            print('Handle  other  error');
+            break;
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text('Caption Image Widget'),
-    );
+    // TODO: implement build
+    if (!controller.value.isInitialized){
+      return Container();
+    }
+    return CameraPreview(controller);
   }
 }
--- lib/view/dobjView.dart
+++ lib/view/dobjView.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:app_a/pages/imageTextPage.dart' as page;
-import 'package:app_a/parts/captionImageWidget.dart' as parts;
-import 'package:app_a/parts/captionTextWidget.dart' as parts;
+import 'package:app_a/parts/dobjImageWidget.dart' as parts;
+import 'package:app_a/parts/dobjTextWidget.dart' as parts;
 
 class DObjView  extends StatelessWidget{
   DObjView({Key? key}): super(key: key);
   @override
   Widget build(BuildContext context)
   => page.imageTextPage(
-      parts.CaptionImageWidget(),
-      parts.CaptionTextWidget()
+      parts.DObjImageWidget(),
+      parts.DObjTextWidget()
   );
 }
